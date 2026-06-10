4c9d9a953866233c1b0ef9b1f62b67ce55bd8c17
Harshit Arora <aroraharshit779@gmail.com>
2026-06-10 11:45:11 +0530

    Configure Express backend to run on Vercel as serverless functions

--- server/server.js
+++ server/server.js
@@ -879,4 +879,8 @@ app.get('/api/users/online', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+if (!process.env.VERCEL) {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+export default app;
